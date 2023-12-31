a2b2504df50c9ab541f185a01a12829e1f8f55ca
Jesus Cadena <jesuscadena27@hotmail.com>
2023-12-31 11:42:09 -0600

    prueba views 200

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("<h1>Mi web personal</h1><h2>Portada</h2>")
