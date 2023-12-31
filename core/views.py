15e3fc763a77692fcf308a51d3d1b5d0eaf59b04
Jesus Cadena <jesuscadena27@hotmail.com>
2023-12-31 12:40:22 -0600

    Creacion de directorio template y plantillas html(about.html, contact.html,home.html,portfolio.html y modif en core/views.py

--- core/views.py
+++ core/views.py
@@ -1,41 +1,18 @@
 from django.shortcuts import render, HttpResponse
-
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about">Acerca de</a></li>
-    <li><a href="/portfolio">Portafolio</a></li>
-    <li><a href="/contact">Mi Contacto</a></li>
-</ul>
-"""
-
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse(html_base + """
-        <h2>Portada</h2>
-        <p>Esto es la portada</p>
-    """)
+    return render(request, "core/home.html")
 
 
 def about(request):
-    return HttpResponse(html_base + """
-        <h2>Acerca de</h2>
-        <p>Me llamo Jesus y soy desarrollador backend</p>
-    """)
+    return render(request, "core/about.html")
 
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-        <h2>Portafolio</h2>
-        <p>Algunos de mis trabajos</p>             
-    """)
+    return render(request, "core/portfolio.html")
 
 
 def contact(request):
-    return HttpResponse(html_base + """
-        <h2>Contacto</h2>
-        <p>Aqui puedes contactar conmigo</p>
-    """)
+    return render(request, "core/contact.html")
