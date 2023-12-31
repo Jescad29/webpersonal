39a763a049565e7993cb45df523d98fc2cbcb1ca
Jesus Cadena <jesuscadena27@hotmail.com>
2023-12-31 12:03:15 -0600

    creacion about, modif home, modif urls y views

--- core/views.py
+++ core/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+<h1>Mi web personal</h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about">Acerca de</a></li>
+</ul>
+"""
+
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("<h1>Mi web personal</h1><h2>Portada</h2>")
+    return HttpResponse(html_base + """
+        <h2>Portada</h2>
+        <p>Esto es la portada</p>
+    """)
+
+
+def about(request):
+    return HttpResponse(html_base + """
+        <h2>Acerca de</h2>
+        <p>Me llamo Jesus y soy desarrollador backend</p>
+    """)
--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -19,6 +19,7 @@ from core import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path("about", views.about, name="about"),
     path('admin/', admin.site.urls),
 
 ]
