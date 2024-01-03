98b39f2d5722e8b1f19a01ae25662a6b491c7530
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-02 23:00:33 -0600

    Se personalizo django admin

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+from django.conf import settings
+
 urlpatterns = [
     path('', views.home, name="home"),
     path("about/", views.about, name="about"),
@@ -25,3 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
